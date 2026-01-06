b64b7e68163b4e909b8beb39a28dc4893460b398
badri-8 <badriprasath.k2462004@gmail.com>
2026-01-06 14:05:01 +0000

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  comments: { type: [String], default: [] }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
 
