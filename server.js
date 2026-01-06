ddb4160f2307433f2a9487bd89ddfd1766e8e64a
badri-8 <badriprasath.k2462004@gmail.com>
2026-01-06 15:57:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,11 +14,5 @@ mongoose.connect(process.env.DB)
 
 require('./routes/api')(app);
 
-// IMPORTANT: only listen if NOT in test mode
-if (process.env.NODE_ENV !== 'test') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log('Listening on port ' + PORT));
-}
-
-// REQUIRED for FCC tests
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Listening on port ' + PORT));
